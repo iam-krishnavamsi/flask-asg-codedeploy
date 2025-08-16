080edc01c5a8621833d6e267bccaef866f611d7b
iam-krishnavamsi <109576412+iam-krishnavamsi@users.noreply.github.com>
2025-08-16 15:34:10 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Running BeforeInstall hook - ensuring Apache2 is installed..."
-if ! [ -x "$(command -v apache2)" ]; then
-    sudo apt update -y
-    sudo apt install -y apache2
-fi
+
+sudo apt update -y
+sudo apt install -y apache2
